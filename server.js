f28e75a77f92173a59038989f298117bb4e20aa8
Ankitdeardigital <ankit@deardigital.com>
2024-05-13 12:22:31 +0530

    feat: Add math module with basic arithmetic functions
    This commit adds a new module called math.js which contains four basic arithmetic functions: add, sub, mul, and div. These functions can be used to perform addition, subtraction, multiplication, and division operations respectively.
    
    The math module is imported in server.js and the functions are called to perform arithmetic operations on the numbers 10 and 20.
    
    This change improves the code by encapsulating the arithmetic operations in a separate module, making the code more modular and reusable.

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const os = require ('os')
 const path = require ('path')
+const { add, sub, mul, div } = require('./math') 
 
 
-console.log("hello");
-console.log(os.type());
-console.log(os.homedir());
-console.log(os.version());
 
-console.log(__dirname);
-console.log(__filename);
+console.log(add(10,20));
+console.log(sub(10,20));
+console.log(mul(10,20));
+console.log(div(10,20));
 
-console.log( path.dirname(__filename));
-console.log( path.basename(__filename));
-console.log( path.extname(__filename));
+
+// console.log("hello");
+// console.log(os.type());
+// console.log(os.homedir());
+// console.log(os.version());
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log( path.dirname(__filename));
+// console.log( path.basename(__filename));
+// console.log( path.extname(__filename));
